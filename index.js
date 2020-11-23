316ab65041b29f602bcab35b1d13f115b7551f68
fredenck <fredzhang2013@gmail.com>
2020-11-22 21:48:26 -0800

    heroku

--- index.js
+++ index.js
@@ -6,8 +6,27 @@ const client = new Discord.Client();
 
 const prefix = "~"
 
+// const http = require('http');
+
+// const hostname = 'localhost';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World!\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
 client.on("message", function(message) { 
 	if (message.author.bot) return;
+	if (message.content.toLowerCase() === "poggers" || message.content.toLowerCase() === "pog" || message.content.toLowerCase() === "pogchamp"){
+ 		// message.react('👁');
+		message.react('<:PogChamp:779889662428577802>>');
+	}
 	if (!message.content.startsWith(prefix)) return;
 
 	const commandBody = message.content.slice(prefix.length);
@@ -16,13 +35,18 @@ client.on("message", function(message) {
 
 	if (command === "ping"){
 		const timeTaken = Date.now() - message.createdTimestamp;
-		message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+		message.reply(`Poong! This message had a latency of ${timeTaken}ms.`);
 	}
 
 	else if (command === "sum") {
     	const numArgs = args.map(x => parseFloat(x));
     	const sum = numArgs.reduce((counter, x) => counter += x);
     	message.reply(`The sum of all the arguments you provided is ${sum}!`);
+ 	}
+
+ 	else if (command === "poggers"){
+ 		// message.react('👁');
+ 		message.react('<:PogChamp:779887726408630294>');
  	}
 });                                      
 
